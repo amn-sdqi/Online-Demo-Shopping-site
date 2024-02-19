16aa8209e1c4fbaf8ae566aba9856ec80a97bb31
amn-sdqi <amaanshahid2510@gmail.com>
2024-02-19 20:12:54 +0530

    Connected to MongoDB

---
+++ app.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const express = require("express");
+const authRoutes = require("./routes/auth.route");
+const db = require("./data/database");
+const { log } = require("console");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
+
+app.use(authRoutes);
+
+db.connect_database()
+	.then(function () {
+		app.listen(3000);
+	})
+	.catch(function () {
+		console.log("Faild to connect to the database");
+		console.log("ERROR");
+	});
